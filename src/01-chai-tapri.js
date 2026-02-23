0bf153c8433ce1c82831f40cac31530b8d8d1396
Uday Patil <udayp9020@gmail.com>
2026-02-23 22:35:56 +0530

    chai Revenue Calculator Question solved

--- src/01-chai-tapri.js
+++ src/01-chai-tapri.js
@@ -28,4 +28,24 @@
  */
 export function chaiTapriRevenue(customers) {
   // Your code here
+ if (
+    typeof customers !== "number" ||
+    !Number.isInteger(customers) ||
+    customers <= 0
+  ) {
+    return { totalChai: 0, totalRevenue: 0 };
+  }
+
+  const totalChai = customers;
+
+  const adrakChai = Math.floor(customers / 3);
+
+  const cuttingChai = customers - adrakChai;
+
+  const totalRevenue = (cuttingChai * 10) + (adrakChai * 15);
+
+  return {
+    totalChai,
+    totalRevenue
+  };
 }
