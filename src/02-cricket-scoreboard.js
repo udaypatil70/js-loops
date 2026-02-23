a45b410d4bed0140d8a00ff7f5e2a777a5869c1a
Uday Patil <udayp9020@gmail.com>
2026-02-23 22:37:11 +0530

    Gully cricket Scoreboard Question solved

--- src/02-cricket-scoreboard.js
+++ src/02-cricket-scoreboard.js
@@ -31,5 +31,48 @@
  *   // => { totalRuns: 0, totalBalls: 0, wickets: 0, fours: 0, sixes: 0 }
  */
 export function cricketScoreboard(balls) {
-  // Your code here
+
+  if (!Array.isArray(balls) || balls.length === 0) {
+    return {
+      totalRuns: 0,
+      totalBalls: 0,
+      wickets: 0,
+      fours: 0,
+      sixes: 0
+    };
+  }
+
+  let totalRuns = 0;
+  let totalBalls = 0;
+  let wickets = 0;
+  let fours = 0;
+  let sixes = 0;
+
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+
+    totalBalls++;
+
+    if (ball === -1) {
+      wickets++;
+
+      if (wickets === 10) {
+        break;
+      }
+
+    } else {
+      totalRuns += ball;
+
+      if (ball === 4) fours++;
+      if (ball === 6) sixes++;
+    }
+  }
+
+  return {
+    totalRuns: totalRuns,
+    totalBalls: totalBalls,
+    wickets: wickets,
+    fours: fours,
+    sixes: sixes
+  };
 }
